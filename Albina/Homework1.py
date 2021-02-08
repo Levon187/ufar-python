c7d12228cb02e33a6992cfc44ab702c002b7afd0
Albina Hakobyan <albina.hakobyan7@gmail.com>
2021-02-08 11:24:27 +0400

    add Albina's homework

---
+++ Albina/Homework1.py
@@ -0,0 +1,108 @@
+# Write a short Python function, is_multiple(n, m), that takes two integer
+# values and returns True if n is a multiple of m, that is, n = mi for some
+# integer i, and False otherwise.
+
+
+# a=int(input("Please enter an integer"))
+# b=int(input("Please enter the second integer"))
+# i=3
+# def is_multiple(n, m):
+#
+#     if n%m==0:
+#         # TODO
+#         return True
+#     return  False
+#
+# print(is_multiple(a,b))
+
+
+# Write a short Python function, is_even(k), that takes an integer value and
+# returns True if k is even, and False otherwise. However, your function
+# cannot use the multiplication, modulo, or division operators.
+# k = int(input("Enter a value to check if its even "))
+# def is_even(k):
+#     #TODO
+#
+#     if k%2 == 0:
+#         return True
+#     return False
+#
+# print(is_even(k))
+
+'''def is_even(k):
+    if k & 1:
+        return False
+    return  True
+
+print(is_even(2))
+print(is_even(3))
+
+print(is_even())'''
+
+
+# Write a short Python function, minmax(data), that takes a sequence of
+# one or more numbers, and returns the smallest and largest numbers, in the
+# form of a tuple of length two. Do not use the built-in functions min or
+# max in implementing your solution.
+
+'''def minmax(data):
+    largest = data[0]
+    smallest = data[0]
+    for item in data:
+        if item > largest:
+            largest = item
+        elif item < smallest:
+            smallest = item
+    return smallest, largest
+
+
+alpha = [2, 2, 3, 4, 5, 6, 7, 8, 99]
+
+print(minmax(alpha))'''
+
+
+# Write a short Python function that takes a positive integer n and returns
+# the sum of the squares of all the positive integers smaller than n.
+
+'''def sum_of_squares(n):
+    n -= 1
+    total = 0
+    while n > 0:
+        total += n * n
+        n -= 1
+    return total
+
+
+print('sum of squares')
+print(sum_of_squares(4))'''
+
+
+#Second version
+def sum_square(n):
+    result = 0
+    if n > 0:
+        for i in range(n):
+            result += i * i
+        return result
+    elif n <= 0:
+        return ("n should be positive")
+
+print(sum_square(5))
+
+
+# Give a single command that computes the sum from Exercise R-1.4, relying
+# on Python’s comprehension syntax and the built-in sum function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Albina/main.py
@@ -0,0 +1,36 @@
+# def is_multiple(n,m):
+#     """ Checks if n is a factor of m where n <= m """
+#     try:
+#         if m%n == 0:
+#             print ("Yes! {0} is a factor of {1}".format(n,m))
+#         else:
+#             print("\n Nope! {0} is NOT a factor of {1}".format(n,m))
+#     except Exception as excpt:
+#         print ("Error!", excpt)
+#     return
+#
+# is_multiple(15,3
+
+
+# 1.2) Write a short Python function, is even(k), that takes an integer value and
+# returns True if k is even, and False otherwise. <em>However, your function
+
+# d pr=1
+#
+#     for i in range(2, x+1):
+#         pr=pr*i
+#     return pr
+#
+# def soch(n,k):
+#     return factorial(n)/(factorial(k)*factorial(n-k))
+#
+# print(soch(5,3))ef factorial(x):
+
+
+
+
+
+
+
+
+
