d09f4938f777c2a04ec9b479f6447e7a561d9525
Hrant Aralelyan <hrantarakel@gmail.com>
2021-04-14 13:23:47 +0400

    Update csv version and add json version

--- Hrant/rates_am.py
+++ Hrant/rates_am.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import json
 
 
 def get_rates_am(url):
@@ -17,7 +18,6 @@ def get_rates_am(url):
         soup = BeautifulSoup(req.text, 'html.parser')
 
         i = 0
-        p = 0
         for a in soup.find_all(name='table', attrs={'id': 'rb', 'class': 'rb'}):
             for b in a.find_all(name='tr'):
                 for c in b.find_all(name='td'):
@@ -27,7 +27,7 @@ def get_rates_am(url):
                     if 17 <= i <= 225 and i % 13 == 4:
                         keys.append(elem[1:])
 
-                    if (0 <= i % 13 <= 2 or 8 <= i % 13 <= 12) and i >= 21 and p <= 16 and i <= 236:
+                    if (0 <= i % 13 <= 2 or 8 <= i % 13 <= 12) and 21 <= i <= 236:
                         temp.append(elem)
                         i += 1
                         continue
@@ -45,7 +45,7 @@ def get_rates_am(url):
             bank_dict[i] = temp2
             temp2 = []
 
-        print(bank_dict)
+        # print(bank_dict)
         new_list = []
         fieldnames = ["Բանկի անվանում", "Առք(USD)", "Վաճառք(USD)", "Առք(EUR)", "Վաճառք(EUR)", "Առք(RUR)", "Վաճառք(RUR)",
                       "Առք(GBP)", "Վաճառք(GBP)"]
@@ -53,7 +53,7 @@ def get_rates_am(url):
         with open('rates_am.csv', 'w', encoding='utf-8', newline='') as rates:
             # fieldnames = [key for key in new_dict.keys()]
             writer = csv.writer(rates)
-            writer.writerow(fieldnames)  # this will add column names
+            writer.writerow(fieldnames)
 
             for key, value in bank_dict.items():
                 new_list.append(key)
@@ -61,6 +61,28 @@ def get_rates_am(url):
                     new_list.append(i)
                 writer.writerow(new_list)
                 new_list = []
+
+        list_of_dicts = []
+        temp_dict = {}
+        with open('rates_am.csv', 'rt', encoding='utf-8')as f:
+            data = csv.reader(f)
+            data = list(data)
+            headers = data[0]
+
+            for row in data:
+                if row != headers:
+                    for i in range(len(headers)):
+                        temp_dict[headers[i]] = row[i]
+
+                    list_of_dicts.append(temp_dict)
+                    temp_dict = {}
+
+        # print(list_of_dicts)
+
+        with open('rates_am.json', 'w', encoding='utf-8') as out_file:
+            # indent is an optional parameter that adds spaces for readability
+            # sort_keys is an optional parameter to save json keys in alphabetical order
+            json.dump(list_of_dicts, out_file, indent=4)
 
 
 url = "http://rates.am/"
