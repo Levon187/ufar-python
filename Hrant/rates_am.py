357b56371d0fcaaefc811bb3f33b3ab6748159f1
Hrant Aralelyan <hrantarakel@gmail.com>
2021-04-14 17:26:45 +0400

    Sort the dict and update csv and json files

--- Hrant/rates_am.py
+++ Hrant/rates_am.py
@@ -18,21 +18,21 @@ def get_rates_am(url):
         soup = BeautifulSoup(req.text, 'html.parser')
 
         i = 0
-        for a in soup.find_all(name='table', attrs={'id': 'rb', 'class': 'rb'}):
-            for b in a.find_all(name='tr'):
-                for c in b.find_all(name='td'):
-                    elem = c.text
-                    # print(elem, f"{i} iteration")
+        a = soup.find(name='table', attrs={'id': 'rb', 'class': 'rb'})
+        for b in a.find_all(name='tr'):
+            for c in b.find_all(name='td'):
+                elem = c.text
+                # print(elem, f"{i} iteration")
 
-                    if 17 <= i <= 225 and i % 13 == 4:
-                        keys.append(elem[1:])
+                if 17 <= i <= 225 and i % 13 == 4:
+                    keys.append(elem[1:])
 
-                    if (0 <= i % 13 <= 2 or 8 <= i % 13 <= 12) and 21 <= i <= 236:
-                        temp.append(elem)
-                        i += 1
-                        continue
-
+                if (0 <= i % 13 <= 2 or 8 <= i % 13 <= 12) and 21 <= i <= 236:
+                    temp.append(elem)
                     i += 1
+                    continue
+
+                i += 1
 
         temp2 = []
         k = 0
@@ -45,44 +45,41 @@ def get_rates_am(url):
             bank_dict[i] = temp2
             temp2 = []
 
-        # print(bank_dict)
+        sorted_dict = {}
+        # sorting the dict by values but storing the keys as a list
+        sorted_values = sorted(bank_dict, key=bank_dict.get, reverse=True)
+        for elem in sorted_values:
+            sorted_dict[elem] = bank_dict[elem]
+
         new_list = []
         fieldnames = ["Բանկի անվանում", "Առք(USD)", "Վաճառք(USD)", "Առք(EUR)", "Վաճառք(EUR)", "Առք(RUR)", "Վաճառք(RUR)",
                       "Առք(GBP)", "Վաճառք(GBP)"]
 
-        with open('rates_am.csv', 'w', encoding='utf-8', newline='') as rates:
-            # fieldnames = [key for key in new_dict.keys()]
-            writer = csv.writer(rates)
+        with open('buy_usd.csv', 'w', encoding='utf-8', newline='') as buy:
+            writer = csv.writer(buy)
             writer.writerow(fieldnames)
 
-            for key, value in bank_dict.items():
+            for key, value in sorted_dict.items():
                 new_list.append(key)
                 for i in value:
                     new_list.append(i)
                 writer.writerow(new_list)
                 new_list = []
 
-        list_of_dicts = []
         temp_dict = {}
-        with open('rates_am.csv', 'rt', encoding='utf-8')as f:
+
+        with open('buy_usd.csv', 'rt', encoding='utf-8') as f:
             data = csv.reader(f)
             data = list(data)
             headers = data[0]
 
-            for row in data:
-                if row != headers:
-                    for i in range(len(headers)):
-                        temp_dict[headers[i]] = row[i]
+            for i in range(len(headers)):
+                temp_dict[headers[i]] = data[1][i]
 
-                    list_of_dicts.append(temp_dict)
-                    temp_dict = {}
-
-        # print(list_of_dicts)
-
-        with open('rates_am.json', 'w', encoding='utf-8') as out_file:
+        with open('buy_usd.json', 'w', encoding='utf-8') as out_file:
             # indent is an optional parameter that adds spaces for readability
             # sort_keys is an optional parameter to save json keys in alphabetical order
-            json.dump(list_of_dicts, out_file, indent=4)
+            json.dump(temp_dict, out_file, indent=4, ensure_ascii=False)
 
 
 url = "http://rates.am/"
